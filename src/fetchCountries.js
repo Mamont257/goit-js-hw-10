e6a06723f91170bd6873d7fdb40a55132cb7f539
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-01-02 14:17:58 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,8 +3,12 @@ fetch('https://restcountries.com/v2/all');
 // console.log(resp);
 // export  default resp.then(data => data.json()).then(res => console.log(res[0]));
 
- export const fun = function fetchCountries(name) {
+ export function fetchCountries(name) {
      console.log(name);
-     const resp = fetch('https://restcountries.com/v2/all');
-     return resp.then(data => data.json()).then(res => res[0])
-}
+     const resp = fetch(`https://restcountries.com/v2/name/${name}`);
+     return resp.then(data => data.json()).catch(err => console.log(err))
+ }
+
+
+
+//  .filter((re) => re.name = name)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import './css/styles.css';
-import {fun} from './fetchCountries'
+import { fetchCountries } from './fetchCountries'
+import Debounce from "lodash.debounce";
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const country = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+// console.log(countryInfo);
+
+input.addEventListener('input', new Debounce(onClick, DEBOUNCE_DELAY));
+
+function onClick(evt) {
+    console.dir(evt.target.value);
+    fetchCountries(evt.target.value.trim()).then(data => data.map(dat => addList(dat)));
+}
+
+function addList(arr) {
+    console.log(arr);
+    // const li = arr.map(ar => ar.name);
+    // console.log(li);
+    country.insertAdjacentHTML('beforeend', `<li class="country-item">${arr.name}</li>`)
+}
 
 
 // console.log(rt.then(data => data.json()).then(res => console.log(res[0])));
 
-console.log(fun());
+// fetchCountries("And").then(data => data.map(dat => console.log(dat)));
