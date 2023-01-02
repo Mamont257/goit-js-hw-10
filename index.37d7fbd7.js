3d09b5aaa1c22c5da489a519fd93658e30d9a162
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-01-02 12:18:30 +0000

    Deploying to gh-pages from @ Mamont257/goit-js-hw-10@e6a06723f91170bd6873d7fdb40a55132cb7f539 🚀

--- index.37d7fbd7.js
+++
@@ -1,2 +0,0 @@
-!function(){fetch("https://restcountries.com/v2/all");var t;console.log((console.log(t),fetch("https://restcountries.com/v2/all").then((function(t){return t.json()})).then((function(t){return t[0]}))))}();
-//# sourceMappingURL=index.37d7fbd7.js.map
--- index.ed49aec0.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/all");console.log(function(t){console.log(t);return fetch("https://restcountries.com/v2/all").then((t=>t.json())).then((t=>t[0]))}());
-//# sourceMappingURL=index.ed49aec0.js.map
