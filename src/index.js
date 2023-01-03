16cef2b6b6983d0a361383e31759b42e592716d6
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-01-04 00:14:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { fetchCountries } from './fetchCountries'
 import Debounce from "lodash.debounce";
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 const country = document.querySelector(".country-list");
@@ -13,25 +13,11 @@ const countryInfo = document.querySelector(".country-info");
 input.addEventListener('input', new Debounce(onClick, DEBOUNCE_DELAY));
 
 function onClick(evt) {
-    fetchCountries(evt.target.value.trim()).then(data => addList(data)).catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
-    const lid = document.querySelector(".country-item");
-
-
-
-
-    //Використав таку конструкцію, технічно працює, але так погано
-
-    if (lid) {
-        for (let i = 0; i <= 10; i++){
-            const lid = document.querySelector(".country-item");
-            lid.remove();
-        }
-    }
-
-
-
-
-
+    fetchCountries(evt.target.value.trim()).then(data => addList(data)).catch(() => 
+    {
+        if (!" ") {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+    }});
 }
 
 function addList(arr) {
@@ -39,15 +25,20 @@ function addList(arr) {
 
     if (arr.length > 10) {
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        country.innerHTML = '';
+        countryInfo.innerHTML = '';
     } else if (arr.length > 1) {
+        countryInfo.innerHTML = '';
         const li = arr.map(({ flags, name }) => `<li class="country-item"><img src="${flags.svg}" alt="" class="country-img" width="25px">${name}</li>`).join('');
-        country.insertAdjacentHTML('beforeend', li);
+        country.innerHTML = li;
     } else {
+        country.innerHTML = '';
+        countryInfo.innerHTML = '';
         const div = arr.map(({ capital, population, languages, flags, name }) =>
             `<li class="country-item"><img src="${flags.svg}" alt="" class="country-img" width="25px">${name}</li>
         <div class="country-item">Capital: ${capital}</div>
         <div class="country-item">Population: ${population}</div>
         <div class="country-item">Languages: ${languages.map(lan => lan.name)}</div>`).join('');
-        countryInfo.insertAdjacentHTML('beforeend', div)
+        countryInfo.innerHTML = div;
     }
 }
