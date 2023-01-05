9bc914080e4f99705c63662032c0e19780f41957
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-01-05 19:44:23 +0200

    fixed

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,10 @@
  export function fetchCountries(name) {
-    //  console.log(name);
      const resp = fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`);
-     return resp.then(data => data.json()).catch(err => console.log(err))
+     return resp.then(resp => {
+         if (resp.ok) {
+            return resp.json()
+         } else {
+            throw new Error(resp.statusText)
+         }
+     })
  }
